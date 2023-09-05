a100d3677d81511eb082fb619bfc6d7acc0375d1
Alikam <witalik372@mail.ru>
2023-09-05 16:22:06 +0300

    second commit

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('myauth/', include('app_auth.urls')),
+    path('', include('app_advertisements.urls'))
